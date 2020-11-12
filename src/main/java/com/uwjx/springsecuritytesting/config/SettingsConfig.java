e0dae4cfdc0af239e20cfcd6d9fc56804ae89673
Hugh Wang <18501667737@163.com>
2020-11-12 19:45:39 +0800

    by Hugh

--- src/main/java/com/uwjx/springsecuritytesting/config/SettingsConfig.java
+++ src/main/java/com/uwjx/springsecuritytesting/config/SettingsConfig.java
@@ -1,9 +1,32 @@
 package com.uwjx.springsecuritytesting.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
+@Validated
 @Configuration
 @ConfigurationProperties(value = "uwjx")
 public class SettingsConfig {
+
+    @Setter
+    @Getter
+    @Valid
+    private Jwt jwt = new Jwt();
+
+
+    @Setter
+    @Getter
+    public static class Jwt{
+        @Min(5000L)
+        private long accessTokenExpireTime = 60 * 1000L; // Access Token 过期时间
+
+        @Min(3600000L)
+        private long refreshTokenExpireTime = 30 * 24 * 3600 * 1000L; // Refresh Token 过期时间
+    }
 }
--- src/main/java/com/uwjx/springsecuritytesting/controller/TestController.java
+++ src/main/java/com/uwjx/springsecuritytesting/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.uwjx.springsecuritytesting.controller;
 
+import com.uwjx.springsecuritytesting.config.SettingsConfig;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,17 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @Slf4j
 @RequestMapping(value = "test")
 public class TestController {
+
+    @Resource
+    private SettingsConfig settingsConfig;
 
     @GetMapping(value = "list")
     public String list(){
         log.warn("TestController list 请求处理中 。。。");
 
 
+        long accessTokenExpiredTime = settingsConfig.getJwt().getAccessTokenExpireTime();
 
+        long refreshTokenExpiredTime = settingsConfig.getJwt().getRefreshTokenExpireTime();
+
+        log.warn("TestController list accessTokenExpiredTime : {}" , accessTokenExpiredTime);
+        log.warn("TestController list refreshTokenExpiredTime : {}" , refreshTokenExpiredTime);
 
         return "TestController list ok";
     }
